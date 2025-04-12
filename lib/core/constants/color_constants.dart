ca3abe0a82b2c0060283c656bebbcafb4e2d6c47
Shadab khan <117011709+imshadabkhan@users.noreply.github.com>
2025-04-12 15:02:32 +0500

    colors and fonts addedd

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -4,24 +4,22 @@ import 'package:flutter/material.dart';
 class ColorConstants {
 
 
-  static var primaryColor = const Color(0xffEAA61F);
-  static var blackColor = const Color(0xff0C0C0C);
-  static var fullBlackColor = const Color(0xff000000);
-  static var redColor = const Color(0xffE33629);
-  static var silverColor = const Color(0xffEAEFF2);
-  static var backgroundColor = const Color(0xfff7f7f7);
-  static var selectedNavIcon = const Color(0xff556121);
-  static var hostButtonColor = const Color(0xffBE52101A);
-  static var splash = const Color(0xffBE5210);
-  static var textColor = const Color(0xff545452);
-  static var cardColor = const Color(0xffFFFFFF);
-  static var whiteColor = const Color(0xffFFFFFF);
-  static var greenColor = const Color(0xff319F43);
-  static var upcomingColor = const Color(0xff007BFF);
-  static var inprogressColor = const Color(0xffDFAA00);
-  static var completedColor = const Color(0xff319F43);
-  static var greyColor = const Color(0xffEAEFF2);
-  static var lightOrange = const Color(0xffFBF2ED);
+  static var primaryColor = const Color(0xff014BC4);
+  static var greyTextColor = const Color(0xff727988);
+  static var blackColor = const Color(0xff000000);
+  static var redColor = const Color(0xffFE2E52);
+  static var navbarColor = const Color(0xff1D1929);
+  static var unselectedNavbarColor = const Color(0xff85838B);
+  static var cardLinearGradientColor=const LinearGradient(colors: [
+    Color(0xff014BC4),
+    Color(0xff8509AA),
+    Color(0xffF90274)
+  ]);
+
+
+
+
+
 
 
 
